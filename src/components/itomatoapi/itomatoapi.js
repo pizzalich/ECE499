3bdffbf14fdc5affc934ec2101b23c56b572ea82
pizzalich <marc.bonwick@jp.kpmg.com>
2019-06-14 12:15:13 -0700

    added API to interface with server

---
+++ src/components/itomatoapi/itomatoapi.js
@@ -0,0 +1,71 @@
+export const itomatoGetData = () => {
+  const data = fetch("http://localhost:3000/crud")
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(err => console.log(err));
+  return data;
+};
+
+export const itomatoPostData = (
+  temp = 999,
+  humidity = -1,
+  light = -1,
+  moisture = -1
+) => {
+  fetch("http://localhost:3000/crud", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      temp: temp,
+      humidity: humidity,
+      light: light,
+      moisture: moisture
+    })
+  })
+    .then(response => response.json())
+    .then(item => {
+      if (Array.isArray(item)) {
+        console.log(item);
+      } else {
+        console.log("data type error");
+      }
+    })
+    .catch(err => console.log(err));
+};
+
+export const itomatoGetImage = () => {
+  //TODO THIS IS BROKEN
+  const data = fetch("http://localhost:3000/crud/image")
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(err => console.log(err));
+  return data;
+};
+
+export const itomatoPostImage = (image = {}) => {
+  //TODO THIS IS BROKEN
+  fetch("http://localhost:3000/crud/image", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      image: image
+    })
+  })
+    .then(response => response.json())
+    .then(item => {
+      if (Array.isArray(item)) {
+        console.log(item);
+      } else {
+        console.log("data type error");
+      }
+    })
+    .catch(err => console.log(err));
+};
