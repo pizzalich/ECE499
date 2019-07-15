b134bed7fc2c7c29ea3f3e0c7c9670faf0fc0ba4
pizzalich <marc.bonwick@jp.kpmg.com>
2019-07-14 21:55:32 -0700

    Made the graphs make more sence

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,8 @@ import {
   FlexibleXYPlot,
   XAxis,
   VerticalBarSeries,
+  LineMarkSeries,
+  LineSeries,
   AreaSeries
 } from "react-vis";
 import "./Home.css";
@@ -23,6 +25,45 @@ const myDATA = [
   { x: 12, y: -0.25, color: "#33A2FF" }
 ];
 
+const waterData = [
+  { x: 1, y: 0.1 },
+  { x: 2, y: 0.1 },
+  { x: 3, y: 1 },
+  { x: 4, y: 1 },
+  { x: 5, y: 1 },
+  { x: 6, y: 0.8 },
+  { x: 7, y: 0.5 },
+  { x: 8, y: 0.3 },
+  { x: 9, y: 0.2 },
+  { x: 10, y: 0.1 }
+];
+
+const tempData = [
+  { x: 1, y: 0.1 },
+  { x: 2, y: 0.1 },
+  { x: 3, y: 1 },
+  { x: 4, y: 1 },
+  { x: 5, y: 1 },
+  { x: 6, y: 0.8 },
+  { x: 7, y: 0.5 },
+  { x: 8, y: 0.3 },
+  { x: 9, y: 0.2 },
+  { x: 10, y: 0.1 }
+];
+
+const waterLine = [
+  { x: 1, y: 0.25 },
+  { x: 2, y: 0.25 },
+  { x: 3, y: 0.25 },
+  { x: 4, y: 0.25 },
+  { x: 5, y: 0.25 },
+  { x: 6, y: 0.25 },
+  { x: 7, y: 0.25 },
+  { x: 8, y: 0.25 },
+  { x: 9, y: 0.25 },
+  { x: 10, y: 0.25 }
+];
+
 const photos = [
   "https://media.istockphoto.com/photos/tomato-isolated-on-white-background-picture-id466175630",
   "https://cdn.gearpatrol.com/wp-content/uploads/2019/01/10-Best-Indoor-Plants-Gear-Patrol-zizi.jpg",
@@ -37,18 +78,6 @@ const projection = ["25", "69", "420", "0"];
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.dummyData = [
-      { x: 1, y: 0.1 },
-      { x: 2, y: 0.1 },
-      { x: 3, y: 1 },
-      { x: 4, y: 1 },
-      { x: 5, y: 1 },
-      { x: 6, y: 0.8 },
-      { x: 7, y: 0.5 },
-      { x: 8, y: 0.3 },
-      { x: 9, y: 0.2 },
-      { x: 10, y: 0.1 }
-    ];
   }
 
   getStatus = status => {
@@ -75,14 +104,24 @@ class Home extends Component {
           Water Level
           <FlexibleXYPlot>
             <XAxis />
-            <AreaSeries className="area-series" data={data} color="#FFAA33" />
+            <VerticalBarSeries
+              className="area-series"
+              data={data}
+              color="#FFAA33"
+            />
+            <LineSeries
+              className="line-series"
+              data={waterLine}
+              color="#FF0000"
+            />
           </FlexibleXYPlot>
         </div>
         <div className="padding" />
         <div className="graph">
-          Daiurnal Cycle
+          Temperature and Humidity
           <FlexibleXYPlot xType="time" colorType="literal">
             <VerticalBarSeries data={myDATA} />
+            <LineMarkSeries className="linemark-series-example" data={myDATA} />
           </FlexibleXYPlot>
         </div>
         <div className="padding" />
@@ -104,10 +143,10 @@ class Home extends Component {
     if (this.props.page === "home") {
       return (
         <div className="home">
-          {this.column(photos[0], status[0], this.dummyData, projection[0])}
-          {this.column(photos[1], status[1], this.dummyData, projection[1])}
-          {this.column(photos[1], status[2], this.dummyData, projection[2])}
-          {this.column(photos[3], status[3], this.dummyData, projection[3])}
+          {this.column(photos[0], status[0], waterData, projection[0])}
+          {this.column(photos[1], status[1], waterData, projection[1])}
+          {this.column(photos[1], status[2], waterData, projection[2])}
+          {this.column(photos[3], status[3], waterData, projection[3])}
         </div>
       );
     } else {
