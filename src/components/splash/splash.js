102b3090459a51438a87874197ba226915f31a5d
pizzalich <marc.bonwick@jp.kpmg.com>
2019-07-16 11:38:55 -0700

    added spash screen

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -1,31 +1,54 @@
 import React, { Component } from "react";
-import "./Login.css";
+import "./splash.css";
 
 class Splash extends Component {
   constructor(props) {
     super(props);
-    this.state = { showSplash: 1 };
+    this.state = { showSplash: true };
   }
 
-  onClick = () => {
-    this.setState;
+  onClickHome = () => {
+    this.props.changeDrawGraphs();
+    this.setState({ showSplash: false });
+  };
+
+  onClickInfo = () => {
+    this.props.changeDrawGraphs();
+    this.props.changeActiveComponent("info");
+    this.setState({ showSplash: false });
   };
 
   render() {
-    if (this.state.showSpash) {
-      return (
+    //if (this.state.showSpash === 1) {
+    return (
+      this.state.showSplash && (
         <div className="splash">
           <div className="loading">
-            Welcome to I-tomato
-            <br />
-            This site contains a demo of the I-Tomato web app
+            <img className="logosplash" src="/tomato.png" alt=" " />
+            <div>
+              Welcome to I-tomato
+              <br />
+              This site contains a demo of the I-Tomato web app <br />
+              Please either continue to:
+              <br />
+              <div className="spacersplash" />
+              <div className="buttonbox">
+                <div className="splashbtn" onClick={this.onClickHome}>
+                  <span className="helper">Live App </span>
+                </div>
+                <div className="splashbtn" onClick={this.onClickInfo}>
+                  <span className="helper">Project Information</span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="dismiss">clear splash</div>
         </div>
-      );
-    } else {
-      return null;
-    }
+      )
+    );
+
+    //   } else {
+    //     return null;
+    //   }
   }
 }
 
